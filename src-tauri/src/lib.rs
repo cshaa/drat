72dec7c5b365f2146fd675eb52e2798ad158ad5b
csha <m93a.cz@gmail.com>
2025-06-27 21:07:15 +0200

    feat: move from rspc back to tauri commands

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,67 +1,57 @@
-use std::{
-    path::PathBuf,
-    sync::{Arc, Mutex},
-};
+use std::sync::Mutex;
 
-use rspc::Router;
+use serde::{Deserialize, Serialize};
 use tokio::sync::broadcast;
-use tokio_stream::{wrappers::BroadcastStream, StreamExt};
+use ts_rs::TS;
 
 #[derive(Debug)]
 struct AppState {
     counter: i32,
-    counter_tx: broadcast::Sender<i32>,
 }
 
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+#[derive(TS, Deserialize)]
+#[ts(export)]
+enum Command {
+    Greet(String),
+    Increment,
+    Decrement,
+}
+
+#[derive(TS, Serialize)]
+#[ts(export)]
+enum CommandResult {
+    Greet(String),
+    Increment(i32),
+    Decrement(i32),
+}
+
+#[tauri::command]
+fn command(state: tauri::State<Mutex<AppState>>, which: Command) -> CommandResult {
+    match which {
+        Command::Greet(s) => {
+            CommandResult::Greet(format!("Hello, {}, you have been greeted from Rust!", s))
+        }
+        Command::Increment => {
+            let mut state = state.lock().unwrap();
+            state.counter += 1;
+            CommandResult::Increment(state.counter)
+        }
+        Command::Decrement => {
+            let mut state = state.lock().unwrap();
+            state.counter -= 1;
+            CommandResult::Decrement(state.counter)
+        }
+    }
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    let (counter_tx, _) = broadcast::channel(64);
-    let state = Arc::new(Mutex::new(AppState {
-        counter: 0,
-        counter_tx,
-    }));
-
-    let router = Router::<Arc<Mutex<AppState>>>::new()
-        .config(
-            rspc::Config::new()
-                .set_ts_bindings_header("/* eslint-disable */")
-                .export_ts_bindings(
-                    PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("../src/lib/bindings.gen.ts"),
-                ),
-        )
-        .query("version", |t| t(|_, _: ()| env!("CARGO_PKG_VERSION")))
-        .query("greet", |t| t(|_, name: String| greet(&name)))
-        .mutation("increment", |t| {
-            t(|state, _: ()| {
-                let mut state = state.lock().unwrap();
-                state.counter += 1;
-                state.counter_tx.send(state.counter).unwrap();
-            })
-        })
-        .mutation("decrement", |t| {
-            t(|state, _: ()| {
-                let mut state = state.lock().unwrap();
-                state.counter -= 1;
-                state.counter_tx.send(state.counter).unwrap();
-            })
-        })
-        .subscription("count", |t| {
-            t(|state, _: ()| {
-                let state = state.lock().unwrap();
-                let rx = state.counter_tx.subscribe();
-                BroadcastStream::new(rx).map(|x| x.unwrap())
-            })
-        })
-        .build();
+    let state = Mutex::new(AppState { counter: 0 });
 
     tauri::Builder::default()
-        .plugin(rspc_tauri::plugin(router.arced(), move |_| state.clone()))
+        .manage(state)
         .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![command])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
